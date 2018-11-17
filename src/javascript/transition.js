59e1110234fd2dfcd27f02859ebc76b26d947ba3
Your Name <lulu.zheng@tufts.edu>
2018-11-17 15:30:52 -0500

    button redirect to homepage

--- src/javascript/transition.js
+++ src/javascript/transition.js
@@ -4,22 +4,33 @@ var modal = document.getElementById('myModal');
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// Get the button that closes the modal
+var close = document.getElementById("close");
+
+var cont = document.getElementById("continue");
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
+    $('myModal').fadeIn('slow');
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+close.onclick = function() {
     modal.style.display = "none";
+    window.location.assign("homepage.html");
+}
+
+cont.onclick= function() {
+	modal.style.display = "none";
+	window.location.reload() ;
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+        $(modal).fadeOut(300);
     }
 }
+
