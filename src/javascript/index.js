41df1becf386b6ffd5f5293806df79ceb53c2b02
YiShu Huang <yishuhuang@YiShudeMacBook-Pro.local>
2018-11-29 17:09:17 -0500

    responsive design for homepage and genre page

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,18 +1,15 @@
 
-$(document).ready(function() {
-		$('a.popup').click(function() {
-    
-         //Getting the variable's value from a link 
-    	var loginBox = $(this).attr('href');
 
-    	//Fade in the Popup
-    	$(loginBox).fadeIn(300);
-    
+$(document).ready(function() {
+		
+        $('a.popup').click(function() {
+            $('.login-popup').fadeIn(300);
+ 
     	//Set the center alignment padding + border see css style
-    	var popMargTop = ($(loginBox).height() + 24) / 2; 
-    	var popMargLeft = ($(loginBox).width() + 24) / 2; 
+    	var popMargTop = ($('.login-popup').height() + 24) / 2; 
+    	var popMargLeft = ($('.login-popup').width() + 24) / 2; 
     
-    	$(loginBox).css({ 
+    	$('.login-popup').css({ 
         'margin-top' : -popMargTop,
         'margin-left' : -popMargLeft
     	});
@@ -25,7 +22,6 @@ $(document).ready(function() {
     
     	return false;
 	});
-
 
 
 	// When clicking on the button close, close the popup window
