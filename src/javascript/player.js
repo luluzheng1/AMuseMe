8bda6310c2de9710baa73cace28e7da3cac44f7f
Byron Zhong <byron.zhong@tufts.edu>
2018-12-09 15:46:46 -0500

    Implemented authorization part of the server

--- src/javascript/player.js
+++ src/javascript/player.js
@@ -4,7 +4,7 @@
 function spotifyPlayer(genre, number) {
     this.genre = genre;
     this.numberTracks = number;
-    this.accessToken = "BQDz4PRDIe8sA6mFpxHsQG1SAS4yEvAUcwm_owDD4biEzqCpJ-1eDHLNqJYCgd7Afocig5vf5PrZyESVFBekEvIS9BxwX_krapkeBw_7TBIHPlCM-fcSBqVLLTaF4bpi_GuN7WZarhW9hMIC8yo";
+    this.accessToken = undefined;
     this.counter = 0;
     this.list = [];
 }
@@ -14,35 +14,40 @@ function spotifyPlayer(genre, number) {
  */
 spotifyPlayer.prototype.initList = function() {
     var self = this;
-    $.ajax({
-        url: 'https://api.spotify.com/v1/recommendations',
-        method: 'GET',
-        dataType: 'json',
-        data: {
-            seed_genres: self.genre,
-            market: 'US',
-        },
+    this.getCredential()
+    .done(function(){
+        console.log('Bearer ' + self.accessToken);
+        $.ajax({
+            url: 'https://api.spotify.com/v1/recommendations',
+            method: 'GET',
+            dataType: 'json',
+            data: {
+                seed_genres: self.genre,
+                market: 'US',
+            },
 
-        headers: {
-            'Authorization': 'Bearer ' + self.accessToken
-        }
-
-    })
-    .done(function(data) {
-        console.log(data);
-        for (eachTrack of data.tracks) {
-            if (eachTrack.preview_url !== null) {
-                let currTrack = {
-                    album: eachTrack.album || 'Unknown',
-                    artists: eachTrack.artists || 'Unknown',
-                    preview_url: eachTrack.preview_url,
-                    id: eachTrack.id
-                }
-                self.list.push(currTrack);
+            headers: {
+                'Authorization': 'Bearer ' + self.accessToken
             }
-        }
-        self.numberTracks = self.list.length;
-        console.log(self.list);
+
+        })
+        .done(function(data) {
+            console.log(data);
+            for (eachTrack of data.tracks) {
+                if (eachTrack.preview_url !== null) {
+                    let currTrack = {
+                        album: eachTrack.album || 'Unknown',
+                        artists: eachTrack.artists || 'Unknown',
+                        name: eachTrack.name,
+                        preview_url: eachTrack.preview_url,
+                        id: eachTrack.id
+                    }
+                    self.list.push(currTrack);
+                }
+            }
+            self.numberTracks = self.list.length;
+            console.log(self.list);
+        });
     });
 };
 
@@ -60,6 +65,8 @@ spotifyPlayer.prototype.next = function() {
  */
 spotifyPlayer.prototype.getPlayerURL = function() {
     if (this.counter < this.numberTracks) {
+        console.log(this.list[this.counter].name);
+        console.log(this.list[this.counter].name);
         return this.list[this.counter].preview_url;
     }
     else {
@@ -74,4 +81,23 @@ spotifyPlayer.prototype.checkAnswer = function () {
     console.log(this.list[this.counter].album);
     console.log(this.list[this.counter].artists);
 };
+
+spotifyPlayer.prototype.getCredential = function () {
+    var self = this;
+    var deferred = new $.Deferred();
+
+    $.ajax({
+        url: 'https://peaceful-castle-28546.herokuapp.com/credential',
+        method: 'GET',
+        dataType: 'json',
+        crossOrigin: true
+    })
+    .done(function(data) {
+        console.log(data.access_token);
+        self.accessToken = data.access_token;
+        deferred.resolve();
+    });
+
+    return deferred;
+}
 
