1f037de1f8a4e72435564c9ce1f5a46558ff3c19
YiShu Huang <yishuhuang@YiShudeMacBook-Pro.local>
2018-12-11 00:18:38 -0500

    add back to homepage btn

--- src/javascript/stats.js
+++ src/javascript/stats.js
@@ -5,9 +5,12 @@
 //     {id:2, name:"Mary May", score:"1"}
 // ];
 
+// var backbtn = document.getElementById('tohome');
 
 $(document).ready(function() {
     console.log("here");
+      $('#tohome').click(function(){
+       window.location.assign("homepage.html")});
   var table = new Tabulator(".tabulator", {
     ajaxURL:"https://amuseme-trivia-game.herokuapp.com/history", //change this to the project's website
     ajaxParams:{key1:"username", key2:"score"},
@@ -31,3 +34,6 @@ $(document).ready(function() {
   table.setData();
 
 });
+
+
+    
